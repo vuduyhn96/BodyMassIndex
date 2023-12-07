11318aab2a2a2d001d69c7a2a14daa538706d9cd
vuduyhn96 <vuanhduyhn1996@gmail.com>
2023-12-07 16:54:03 +0700

    BodyMassIndex

---
+++ BodyMassIndex.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.printf("%d is a leap year", year);
+                } else {
+                    System.out.printf("%d is NOT a leap year", year);
+                }
+            } else {
+                System.out.printf("%d is a leap year", year);
+            }
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+    }
+}
